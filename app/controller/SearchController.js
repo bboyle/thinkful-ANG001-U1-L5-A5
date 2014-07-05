df71f0529235958d98fea53b0f58a81baa2a0f4d
Ben Boyle <benjamins.boyle@gmail.com>
2014-07-05 23:33:00 +1000

    hookup search method

--- app/controller/SearchController.js
+++ app/controller/SearchController.js
@@ -3,4 +3,16 @@ angular.module( 'kitten-search' )
 
 	this.query = '';
 
+
+	this.findImages = function findImages() {
+		console.log( 'findImages', this.query );
+		// TODO look up the instagram API
+		// show that instagram is being queried…
+
+		// onfail: display error
+		// on no results: display message
+
+		// on success: update results
+	};
+
 });
