60214cff952f648cef6ddc41a4ffae7813964368
Ben Boyle <benjamins.boyle@gmail.com>
2014-07-07 14:19:29 +1000

    hide search header until query is run

--- app/controller/ResultsController.js
+++ app/controller/ResultsController.js
@@ -1,14 +1,15 @@
 angular.module( 'kitten-search' )
 .controller( 'ResultsController', function( $scope, $attrs, $rootScope ) {
 	var RC = this;
-	this.results = false;
+	this.available = false;
 
-	this.count = 24;
-	this.query = 'kittens';
+	this.count = 0;
+	this.query = '';
 
 	// new results available
 	$scope.$on( 'results', function( event, data ) {
 		console.log( 'updating results', data );
+		RC.available = true;
 		RC.count = data.count;
 		RC.query = data.query;
 		RC.instagrams = data.data;
